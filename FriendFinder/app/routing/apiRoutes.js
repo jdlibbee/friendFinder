3f7698794438218784f5c7a5ccbf5861ef91ecfd
jdlibbee <libbeelibbee@yahoo.com>
2018-07-21 23:43:28 -0600

    last updates

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -5,6 +5,31 @@ module.exports = function (app) {
         res.json(friends);
     });
     app.post("/api/friends", function (req, res) {
-        friends.push(req.body);
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            totalDifference = 0;
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
     })
 }
